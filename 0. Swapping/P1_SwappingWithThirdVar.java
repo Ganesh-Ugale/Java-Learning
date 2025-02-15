8b73a967f5822554468abac47a729d9f1315f647
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-02-15 09:50:08 +0530

    Swapping- 1
    Swapping Two Values Using Third Variable

---
+++ 0. Swapping/P1_SwappingWithThirdVar.java
@@ -0,0 +1,27 @@
+/**
+ * Swapping Values with Third Variable
+ */
+
+class SwappingWithThirdVar
+{
+	public static void main(String[] args)
+	{
+		// Swapping values with Third Variable
+
+		int a = 2;
+		int b = 4;
+		int temp; // 3rd var
+
+		System.err.println("BEFORE SWAPPING a: " + a);
+		System.err.println("BEFORE SWAPPING a: " + b);
+
+		temp = a;
+		a = b;
+		b = temp;
+
+		System.err.println("AFTER SWAPPING a: " + a);
+		System.err.println("AFTER SWAPPING a: " + b);
+
+		
+	}
+}
