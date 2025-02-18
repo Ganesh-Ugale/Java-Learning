6670cff38d44e071bca737b069dc900cc659aff2
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-02-18 14:21:32 +0530

    Short Conversion
    Short Conversion means Storing a Value from smaller container to bigger container i.e. Widening

---
+++ 2. Operators/Assignment Operator/Widening/P2_ShortConversion.java
@@ -0,0 +1,31 @@
+// Short Conversion means Storing a Value from smaller container to bigger container i.e Widening
+
+
+class ShortConversion
+{
+	public static void main(String[] args)
+	{
+		// Short Value is can't Store to Byte Date Type Var 
+
+		short shortValue = 32767; // Stored Successfully
+		System.out.println("Short Max Value: " + shortValue);
+
+		//char charVar = shortValue; // CTE lossy conversion from short to char
+		//System.out.println("Short to char: " + charVar);
+
+		int intVar = shortValue; // Stored Successfully
+		System.out.println("short to int: " + intVar);
+
+		long longVar = shortValue; // Stored Successfully
+		System.out.println("short to long: " + longVar);
+
+		float floatVar = shortValue; // Stored Successfully
+		System.out.println("short to float: " + floatVar);
+		
+		double doubleVar = shortValue; // Stored Successfully
+		System.out.println("short to double: " + doubleVar);
+
+
+
+	}
+}
