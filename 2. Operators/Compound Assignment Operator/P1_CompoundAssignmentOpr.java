3e727384c80a326b0d4d75b9753b881121488dbb
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-02-20 21:06:07 +0530

    Compound Assignment Operator
    How the Compound Assignment Operator Works Left to Right

---
+++ 2. Operators/Compound Assignment Operator/P1_CompoundAssignmentOpr.java
@@ -0,0 +1,17 @@
+class CompoundAssignmentOpr
+{
+	public static void main(String[] args)
+	{
+		int salarry = 50;
+		salarry +=40; // i.e salarry = salarry + 4000;
+		System.out.println(salarry);
+		salarry -=20;// i.e salarry = salarry - 4000;
+		System.out.println(salarry);
+		salarry *=20.100f;// i.e salarry = salarry * 4000;
+		System.out.println(salarry);
+		salarry /=20d;// i.e salarry = salarry / 4000;
+		System.out.println(salarry);
+		salarry %=20;// i.e salarry = salarry % 4000;
+		System.out.println(salarry);
+	}
+}
