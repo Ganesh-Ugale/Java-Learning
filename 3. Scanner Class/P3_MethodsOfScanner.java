edf0e1089ffb9002e5a7c8a6107530ace889e90e
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-02-21 14:16:36 +0530

    Methods Of Scanner Class
    All Methods of Scanner Class with there Examples

---
+++ 3. Scanner Class/P3_MethodsOfScanner.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class MethodsOfScanner
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+
+		byte a = sc.nextByte();
+		short b = sc.nextShort();
+		int c = sc.nextInt();
+		long d = sc.nextLong();
+		float e = sc.nextFloat();
+		double h = sc.nextDouble();
+		// here we need to enter a nextLine() method to clear a buffer
+		String f = sc.nextLine();
+		boolean g = sc.nextBoolean();
+	}
+}
