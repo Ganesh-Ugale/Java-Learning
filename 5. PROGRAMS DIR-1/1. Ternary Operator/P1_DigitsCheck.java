f858c80cadaba69032bebdf11d4210ea5f43d847
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-02-23 21:32:36 +0530

    Digits Check
    Check the user entered char is a Digit or Not

---
+++ 5. PROGRAMS DIR-1/1. Ternary Operator/P1_DigitsCheck.java
@@ -0,0 +1,17 @@
+/*
+ * Check the user entered char is a Digit or Not
+ */
+
+import java.util.Scanner;
+
+class DigitsCheck
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Enter a Digite : ");
+		char ch = new Scanner(System.in).next().charAt(0);
+
+		String op = (ch>='0' && ch<='9')?(ch + " is a digit"):(ch + " is not a digit");
+		System.out.println(op);
+	}
+}
