cfdedb8eff23e615b1a5d590b667947bbdc1efd1
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-02-25 07:57:49 +0530

    Prime Num Check
    Check the Entered Num is Prime Or Not Using Ternary Operator

---
+++ 5. PROGRAMS DIR-1/1. Ternary Operator/P9_PrimeNum.java
@@ -0,0 +1,17 @@
+/*
+  Check the Entered Num is Prime Or Not Using Ternary Operator
+ */
+
+import java.util.Scanner;
+
+class PrimeNum
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Enter a Num : ");
+		int num = new Scanner(System.in).nextInt();
+		
+		String primeNum = (num==1)?("1 is not prime number"):(num==2 || num==3)?(num+"is Prime"):((num%2==0 || num%3==0)?(num+"is not Prime"):(num+"is Prime"));
+		System.out.println(primeNum);
+	}
+}
