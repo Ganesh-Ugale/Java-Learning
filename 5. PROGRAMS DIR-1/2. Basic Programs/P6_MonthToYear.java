7a1bcb9280d3d63f0ac22e5ffbfb9926bf6bd356
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-02-26 21:41:20 +0530

    Month to Years
    Program to Convert the Months to years and remaining Months

---
+++ 5. PROGRAMS DIR-1/2. Basic Programs/P6_MonthToYear.java
@@ -0,0 +1,22 @@
+/*
+ * Program to Convert the Months to years and remaining Months
+ */
+
+import java.util.Scanner;
+class MonthToYear
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter the Month : ");
+		int month = sc.nextInt();
+
+		int year = month/12;
+		int remMonth = month%12;
+		String str = year + "."+remMonth; // Method 1
+		//String str = year+ " Years" + " & "+remMonth + " Months"; // Method 2 direct str print
+		float con = Float.parseFloat(str);
+		System.out.println(con);
+	}
+}
