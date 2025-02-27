ccaf08968602edaa245fa6327fb9cc173e75ab3c
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-02-27 11:37:00 +0530

    Pay Fees Program Using Simple if
    Write an Program of Fees Pay Amount Using Simple if Statement

---
+++ 6. Control Flow Statements/1. Decision Statements/Simple If/P3_PayFees.java
@@ -0,0 +1,20 @@
+/*
+Write an Program of Fees Pay Amount Using Simple if Statement
+ */
+
+import java.util.Scanner;
+class PayFees
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Are You Paid Student? ");
+		boolean ip = new Scanner(System.in).nextBoolean();
+
+		if(ip)
+		{
+			System.out.print("Pay the Fees ");
+		}
+		System.out.println("Do the class");
+
+	}
+}
