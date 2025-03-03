da74e27531ae63a52360580d05311f050549912f
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-03-03 13:48:24 +0530

    Print Lowercase Char z-a
    Print the Lowercase char from z to a using for loop

---
+++ 6. Control Flow Statements/2. Loops/2. For Loop/P3_LoweCaseChar.java
@@ -0,0 +1,13 @@
+/*
+Print the Lowercase char from z to a using for loop
+ */
+class LoweCaseChar
+{
+	public static void main(String[] args)
+	{
+		for (char i = 'z'; i>='a' ; i--)
+		{
+			System.out.println(i);
+		}
+	}
+}
