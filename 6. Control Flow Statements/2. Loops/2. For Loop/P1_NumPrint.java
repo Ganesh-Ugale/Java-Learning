137c0d289a1c5217d0e6775c7359fa47d5f62242
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-03-03 13:46:06 +0530

    Numbers Print
    Print the Numbers from 1 to 10 using for loop

---
+++ 6. Control Flow Statements/2. Loops/2. For Loop/P1_NumPrint.java
@@ -0,0 +1,13 @@
+/*
+Print the Numbers from 1 to 10 using for loop
+ */
+class NumPrint
+{
+	public static void main(String[] args)
+	{
+		for (int i=1; i<=10 ; i++ )
+		{
+			System.out.println(i);
+		}
+	}
+}
