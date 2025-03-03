3678c2472d0f17054eec94bccd07b23f98170190
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-03-03 13:49:18 +0530

    Print Digits
    Program to Print the Digits from 0 to 9 using for loop

---
+++ 6. Control Flow Statements/2. Loops/2. For Loop/P4_Digits.java
@@ -0,0 +1,11 @@
+// Program to Print the Digits from 0 to 9 using for loop
+class Digits
+{
+	public static void main(String[] args)
+	{
+		for (char i = '0'; i<='9' ; i++)
+		{
+			System.out.println(i + " : " + (int)i);
+		}
+	}
+}
