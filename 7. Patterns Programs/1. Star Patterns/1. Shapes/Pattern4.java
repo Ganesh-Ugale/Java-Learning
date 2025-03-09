726edd4244d4dcf6ec04369aa6c1c6fb5200db89
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-03-09 12:09:26 +0530

    Top Line of Square
    Print the Following Pattern: (Top Line of Square)
    
    * * * * *

---
+++ 7. Patterns Programs/1. Star Patterns/1. Shapes/Pattern4.java
@@ -0,0 +1,20 @@
+/*
+Print the Following Pattern: (Top Line of Square)
+
+* * * * *
+
+
+*/
+
+class Pattern4{
+	public static void main(String[] args) {
+		int n = 5; // number of i and j
+		for (int i = 1; i <= n; i++) {  // i (rows)
+			for (int j = 1; j <= n; j++) { // j (columns)
+				if (i==1) 
+					System.out.print("* ");  // Print stars where i == 1
+			}
+			System.out.println();  // Print new Line
+		}
+	}
+}
