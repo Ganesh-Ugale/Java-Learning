385010d29dc2aeb3034778222dec9b9dd4373185
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-03-09 12:08:05 +0530

    Square Pattern
    Print the Following Pattern: (Square)
    
    * * * * *
    * * * * *
    * * * * *
    * * * * *
    * * * * *

---
+++ 7. Patterns Programs/1. Star Patterns/1. Shapes/Pattern1.java
@@ -0,0 +1,21 @@
+/*
+Print the Following Pattern: (Square)
+
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+
+*/
+
+class Pattern1{
+	public static void main(String[] args) {
+		int n = 5; // number i and j
+		for (int i = 1; i <= n; i++) {  // i (rows)
+			for (int j = 1; j <= n; j++)  // j (columns)
+				System.out.print("* "); // Prints the stars + space
+			System.out.println();  // gives new Line after every iteration. i.e cursor sets after each complete j iteration to new line.
+		}
+	}
+}
