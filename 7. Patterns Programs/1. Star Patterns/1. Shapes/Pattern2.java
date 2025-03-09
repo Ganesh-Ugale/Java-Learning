9926adef344580b6b5f8cfacaa4b03d4469b80fe
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-03-09 12:08:36 +0530

    Vertical Rectangle
    Print the Following Pattern: (vertical Rectangle)
    
    * * * *
    * * * *
    * * * *
    * * * *
    * * * *

---
+++ 7. Patterns Programs/1. Star Patterns/1. Shapes/Pattern2.java
@@ -0,0 +1,21 @@
+/*
+Print the Following Pattern: (verticle Rectangle)
+
+* * * * 
+* * * * 
+* * * * 
+* * * * 
+* * * * 
+
+*/
+
+class Pattern2{
+	public static void main(String[] args) {
+		int n = 5; // number of i and j
+		for (int i = 1; i <= n; i++) {  // i (rows)
+			for (int j = 1; j <= (n-1); j++)  // j (columns)
+				System.out.print("* "); // Prints the stars + space
+			System.out.println();  // Print new Line
+		}
+	}
+}
