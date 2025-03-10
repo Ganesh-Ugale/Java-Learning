188b88591a1f12e41b98719196e6176824bbf8b1
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-03-10 09:12:42 +0530

    Hollow Square + left Diagonal
    Print the Following Pattern: (Hollow Square + left Diagonal)
    
    * * * * *
    * *     *
    *   *   *
    *     * *
    * * * * *

---
+++ 7. Patterns Programs/1. Star Patterns/1. Shapes/Pattern8.java
@@ -0,0 +1,25 @@
+/*
+Print the Following Pattern: (Hollow Square + left Diagonal)
+
+* * * * *
+* *     *
+*   *   *
+*     * *
+* * * * *
+
+*/
+
+class Pattern8{
+	public static void main(String[] args) {
+		int n = 5; // number of i and j
+		for (int i = 1; i <= n; i++) {  // i (rows)
+			for (int j = 1; j <= n; j++) { // j (columns)
+				if (i==1 || j==1 || i==n || j==n || i==j) 
+					System.out.print("* ");  // Print stars where i == 1
+				else
+					System.out.print("  "); // where above condition false so print space
+			}
+			System.out.println();  // Print new Line
+		}
+	}
+}
