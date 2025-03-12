9fd8bcfeb70fda31513ee51c9bc0c50296add663
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-03-12 11:08:09 +0530

    Opposite- Right angle Traingle- Right Side
    Print the Following Pattern: (Opposite- Right angle Traingle- Right Side)
    
    * * * * * * *
      * * * * * *
        * * * * *
          * * * *
            * * *
              * *
                *

---
+++ 7. Patterns Programs/1. Star Patterns/1. Shapes/P23_Pattern.java
@@ -0,0 +1,26 @@
+/*
+Print the Following Pattern: (Opposite- Right angle Traingle- Right Side)
+
+* * * * * * *
+  * * * * * *
+    * * * * *
+      * * * *
+        * * *
+          * *
+            *
+
+*/
+
+
+class P23_Pattern{
+	public static void main(String[] args) {
+		int n = 7; // number of i and j
+		for (int i = n; i >= 1; i--) {  // i (rows)
+			for (int j = 1; j <= (n-i); j++) // Space Loop
+				System.out.print("  ");
+			for (int j = 1; j <= i; j++) // j (columns)
+				System.out.print("* "); // Prints the stars + space
+			System.out.println();  // Print new Line
+		}
+	}
+}
