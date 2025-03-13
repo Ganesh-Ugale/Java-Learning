d43f1129e8653a3f719985994f0bca174783e396
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-03-13 23:23:56 +0530

    Traingle- Rotate Clock wise Once
    Print the following Pattern: (Traingle- Rotate Clock wise Once)
    
    *
    * *
    * * *
    * * * *
    * * * * *
    * * * * * *
    * * * * * * *
    * * * * * *
    * * * * *
    * * * *
    * * *
    * *
    *

---
+++ 7. Patterns Programs/1. Star Patterns/1. Shapes/P30_Pattern.java
@@ -0,0 +1,39 @@
+/*
+Print the following Pattern: (Traingle- Rotate Clock wise Once)
+
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * * * *
+* * * * * * *
+* * * * * *
+* * * * *		
+* * * *
+* * *
+* *
+*
+
+*/
+
+class P30_Pattern{
+	public static void main(String[] args) {
+		int n = 7;
+		for (int i = 1; i <= n; i++) { // Columns (i)
+			// for (int space = 1; space <= (n-i); space++) // Space Loop
+			// 	System.out.print("");
+			for (int j = 1; j <= i; j++) // Rows (j)
+				System.out.print("* ");
+			System.out.println();  // newline
+		}
+		// Down side
+		for (int i = (n-1); i >= 1; i--) { // Columns (i)
+			// for (int space = 1; space <= (n-i); space++) // Space Loop
+			// 	System.out.print("");
+			for (int j = 1; j <= i; j++) // Rows (j)
+				System.out.print("* ");
+			System.out.println();  // newline
+		}
+	}
+}
