92710aff9105459c5e475cbae2241d06ad7f2f03
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-03-13 23:21:41 +0530

    RHS- Hollow Right angle Traingle
    Print the following Pattern: (RHS- Hollow Right angle Traingle)
    
                *
              * *
            *   *
          *     *
        *       *
      *         *
    * * * * * * *

---
+++ 7. Patterns Programs/1. Star Patterns/1. Shapes/P26_Pattern.java
@@ -0,0 +1,28 @@
+/*
+Print the following Pattern: (RHS- Hollow Right angle Traingle)
+
+            *
+          * *
+        *   *
+      *     *
+    *       *
+  *         *
+* * * * * * *
+
+*/
+
+class P26_Pattern{
+	public static void main(String[] args) {
+		int n = 7;
+		for (int i = 1; i <= n; i++) { // Columns (i)
+			for (int space = 1; space <= (n-i); space++) // Space Loop
+				System.out.print("  ");
+			for (int j = 1; j <= i; j++) // Rows (j)
+				if (j==1 || i==n || i==j) 
+					System.out.print("* "); // if anyone conditions true then only print star
+				else
+					System.out.print("  "); // if anyone conditions false then only print space
+			System.out.println();  // newline
+		}
+	}
+}
