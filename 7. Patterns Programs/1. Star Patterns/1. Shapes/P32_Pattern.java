5188197088fd0b00d079c35f1882f4ad6f5f8010
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-03-14 13:19:12 +0530

    Hollow-Traingle- Rotate Clock wise Once
    Print the following Pattern: (Hollow-Traingle- Rotate Clock wise Once)
    
    *
    * *
    *   *
    *     *
    *       *
    *         *
    *           *
    *         *
    *       *
    *     *
    *   *
    * *
    *

---
+++ 7. Patterns Programs/1. Star Patterns/1. Shapes/P32_Pattern.java
@@ -0,0 +1,45 @@
+/*
+Print the following Pattern: (Hollow-Traingle- Rotate Clock wise Once)
+
+*
+* *
+*   *
+*     *
+*       *
+*         *
+*           *
+*         *
+*       *
+*     *
+*   *
+* *
+*
+
+*/
+
+class P32_Pattern{
+	public static void main(String[] args) {
+		int n = 7;
+		for (int i = 1; i <= n; i++) { // Columns (i)
+			// for (int space = 1; space <= (n-i); space++) // Space Loop
+			// 	System.out.print("  ");
+			for (int j = 1; j <= i; j++) // Rows (j)
+				if (j==1 || i==j || j==n)
+					System.out.print("* ");
+				else
+					System.out.print("  ");
+			System.out.println();  // newline
+		}
+		// Down side
+		for (int i = (n-1); i >= 1; i--) { // Columns (i)
+			// for (int space = 1; space <= (n-i); space++) // Space Loop
+			// 	System.out.print("  ");
+			for (int j = 1; j <= i; j++) // Rows (j)
+				if (j==1 || i==j || j==n)
+					System.out.print("* ");
+				else
+					System.out.print("  ");
+			System.out.println();  // newline
+		}
+	}
+}
