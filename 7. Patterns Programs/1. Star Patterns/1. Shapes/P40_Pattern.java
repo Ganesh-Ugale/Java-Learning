e94ca99ab9b6b01871886e413df48e08bb827271
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-03-15 22:33:09 +0530

    Square + Clockwise Y
    Print the following Pattern: (Square + Clockwise Y)
    
    * * * * * * * * * * * * *
    *                     * *
    *                   *   *
    *                 *     *
    *               *       *
    *             *         *
    * * * * * * *           *
    *             *         *
    *               *       *
    *                 *     *
    *                   *   *
    *                     * *
    * * * * * * * * * * * * *

---
+++ 7. Patterns Programs/1. Star Patterns/1. Shapes/P40_Pattern.java
@@ -0,0 +1,34 @@
+/*
+Print the following Pattern: (Square + Clockwise Y)
+
+* * * * * * * * * * * * *
+*                     * *
+*                   *   *
+*                 *     *
+*               *       *
+*             *         *
+* * * * * * *           *
+*             *         *
+*               *       *
+*                 *     *
+*                   *   *
+*                     * *
+* * * * * * * * * * * * *
+
+*/
+
+class P40_Pattern{
+	public static void main(String[] args) {
+		int n = 13;
+		for (int i = 1; i <= n; i++) { // Columns (i)
+			// for (int space = 1; space <= (n-i); space++) // Space Loop
+			// 	System.out.print(" ");
+			for (int j = 1; j <= n; j++) // Rows (j)
+				if (i==1 || j==1 || i==n || j==n || (i+j==n+1 && j>=n/2+1) || (i==j && j>=n/2+1) || (i==n/2+1 && j<=n/2+1)) 
+					System.out.print("* "); // if anyone conditions true then only print star
+				else
+					System.out.print("  "); // if anyone conditions false then only print space
+			System.out.println();  // newline
+		}
+	}
+}
