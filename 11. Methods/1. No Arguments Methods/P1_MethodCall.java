e28ab16312770fd7b0acf80804ce48531e6b3284
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-03-24 21:53:12 +0530

    Method Call
    How User Defined Method Call in Main method

---
+++ 11. Methods/1. No Arguments Methods/P1_MethodCall.java
@@ -0,0 +1,12 @@
+// How User Defined Method Call in Main method
+
+class P1_MethodCall{
+	public static void main(String[] args) {
+		test(); // Method Calling		
+	}
+
+	// No Arguments Method
+	public static void test(){ // Method Declaration Statement
+		System.out.println("Hello Methods in Java!"); // Mehthod Body
+	}
+}
