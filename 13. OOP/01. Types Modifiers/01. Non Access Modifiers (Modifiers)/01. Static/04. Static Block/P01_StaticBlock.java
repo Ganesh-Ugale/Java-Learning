e31e8805dd30d5f2b321afbc1292df77cf3162f8
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-04-02 09:12:26 +0530

    Static Block
    1. We Can Executes a Block of Code Before the main method Execution Starts.
    i.e it executes during the class loading process.
    2. Such as Variable, methods, or any static member of class.
    3. NOTE: We Can Execute a Stack block or any member of class before the main()
       Method, but, We Can't Run a Program Without Main Method, So Main method is IMP for executing a Program Successfully.

---
+++ 13. OOP/01. Types Modifiers/01. Non Access Modifiers (Modifiers)/01. Static/04. Static Block/P01_StaticBlock.java
@@ -0,0 +1,28 @@
+/*
+1. We Can Executes a Block of Code Before the main method Execution Starts.
+i.e it executes during the class loading process. 
+2. Such as Variable, methods, or any static member of class.
+3. NOTE: We Can Execute a Stack block or any member of class before the main() 
+   Method, but, We Can't Run a Program Without Main Method, So Main method is IMP for executing a Program Successfully.
+*/
+
+class P01_StaticBlock{
+	// Static Block
+
+	// Declare Before the main method Writes
+	static{
+		System.out.println("\nStatic Block 1 Executed!"); // Executes First bcz Static Members are Executes TOP to BOTTOM Priority Wise.
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("\nMain Method Executed!\n");
+	}
+
+	// Declare After the main Method Writes
+	static{
+		System.out.println("Static Block 2 Executed!"); // Executes 2nd
+	}
+	static{
+		System.out.println("Static Block 3 Executed!"); // Executes 3rd
+	}
+}
