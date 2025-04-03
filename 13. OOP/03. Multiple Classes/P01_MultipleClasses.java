5792d330d24e6391a1fd799ede35bf362742fdf8
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-04-03 23:23:33 +0530

    Multiple Classes
    Multiple Classes:
    
    1. When a Soruce code contains only multiple default classes then We can give
       any name to the file.
    2. When a Java Source File, Contains multiple classes & when Java Compiler
       Compile a file then one class file creates for each class present in the File.
    3. Note:  A Java File duplicate classs name not allowed.
    4. A class names not have any access modifier thoes class names called as
       default classes.

---
+++ 13. OOP/03. Multiple Classes/P01_MultipleClasses.java
@@ -0,0 +1,36 @@
+/*
+Multiple Classes:
+
+1. When a Soruce code contains only multiple default classes then We can give 
+   any name to the file.
+2. When a Java Source File, Contains multiple classes & when Java Compiler 
+   Compile a file then one class file creates for each class present in the File. 
+3. Note:  A Java File duplicate classs name not allowed.
+4. A class names not have any access modifier thoes class names called as 
+   default classes.
+
+*/
+
+class Class_A{
+	public static void main(String[] args) {
+		System.out.println("From Class A!");
+	}
+}
+
+class Class_B{
+	public static void main(String[] args) {
+		System.out.println("From Class B!");
+	}
+}
+
+class Class_C{
+	public static void main(String[] args) {
+		System.out.println("From Class C!");
+	}
+}
+
+class Class_D{
+	public static void main(String[] args) {
+		System.out.println("From Class D!");
+	}
+}
