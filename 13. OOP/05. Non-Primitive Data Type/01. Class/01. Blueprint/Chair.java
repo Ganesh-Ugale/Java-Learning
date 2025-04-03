2d376a280492be4f36efa34a272cc97c73ee5f39
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-04-03 23:24:58 +0530

    Blueprint Class
    1. A blueprint of an object is written inside the class.
    2. It is a Blueprint of Object.
    3. Inside the Blueprint of Object the non-static var(Attributes/
       Propertiesz) and non-static methods(actions/behaviours).

---
+++ 13. OOP/05. Non-Primitive Data Type/01. Class/01. Blueprint/Chair.java
@@ -0,0 +1,16 @@
+/*
+1. A blueprint of an object is written inside the class.
+2. It is a Blueprint of Object.
+3. Inside the Blueprint of Object the non-static var(Attributes/
+   Propertiesz) and non-static methods(actions/behaviours).
+*/
+
+class Chair{  /*class is keyword and class name is a non-primitive data type.*/
+
+
+	// NOTE: Here We Did Not Write any Main() Method
+	
+}
+
+
+// Here we create a simple class as name Chair
