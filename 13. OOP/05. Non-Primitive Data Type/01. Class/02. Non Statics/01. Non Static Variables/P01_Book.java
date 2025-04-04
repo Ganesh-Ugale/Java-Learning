16f3e10e9bc8a1b2fc627a619298729f6e6055a0
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-04-04 22:50:35 +0530

    Book
    1. Creating a blueprint of Book Object inside the P01_Book Class.
    2. Members Which are declared in the class block & not prifix with static
       modifier are known as non-static members.

---
+++ 13. OOP/05. Non-Primitive Data Type/01. Class/02. Non Statics/01. Non Static Variables/P01_Book.java
@@ -0,0 +1,16 @@
+/*
+1. Creating a blueprint of Book Object inside the P01_Book Class. 
+2. Members Which are declared in the class block & not prifix with static
+   modifier are known as non-static members.
+*/
+
+class P01_Book{  // P01_Book is a Non- Primitive Data Type.
+	/*These all are the non-static variables(attributs/properties of objects). */
+	String brand; // brand is attribute of object
+	double price;
+	int noOfPages;
+	String type;
+	int weight;
+	String bound;
+
+}
