c23a2ccc64efbd0120dbdba93f9e09494edc5fc5
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-04-07 22:16:22 +0530

    Book Driver
    Book Object Driver Class

---
+++ 13. OOP/05. Non-Primitive Data Type/01. Class/02. Non Statics/03. Non Static Initializers/01. Non Static Declare & Initialization Statement/P01_BookDriver.java
@@ -0,0 +1,16 @@
+class P01_BookDriver{
+	public static void main(String[] args) {
+		
+		// Here we Create an Object with name b1 
+		P01_Book b1 = new P01_Book();
+		b1.displayBook();
+
+		System.out.println("------------------------------------");
+
+		// Here we Create an Object with name b2
+		P01_Book b2 = new P01_Book();
+		b2.displayBook();
+
+
+	}
+}
