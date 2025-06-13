62dbedd7df4f8bb587fd43aca5aa4fafbaf976d5
Ganesh-Ugale <ugale.ganesh.d@gmail.com>
2025-06-13 22:06:10 +0530

    Parent Class
    Understanding the Concept of is A Relationship

---
+++ 13. OOP/07. Principal's Of OOP's/relationshipsInJava/src/isARelationship/ParentClassA.java
@@ -0,0 +1,14 @@
+package isARelationship;
+
+public class ParentClassA {
+	
+	int a = 5;
+	
+	public void displayA() {
+		System.out.println(a);
+	}
+	
+	public ParentClassA() {
+		// TODO Auto-generated constructor stub
+	}
+}
