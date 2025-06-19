1a06197d82dcb89fd515523002d0053b3ed00426
Ganesh-Ugale <ugale.ganesh.d@gmail.com>
2025-06-19 22:47:18 +0530

    Doctor Class
    Using Super Call Statement

---
+++ 13. OOP/07. Principal's Of OOP's/PrinciplesOfOpps/src/supercallstatement/Doctor.java
@@ -0,0 +1,37 @@
+package supercallstatement;
+
+public class Doctor {
+//	Properties
+	String docName;
+	double fees;
+	String degree;
+	int experience;
+	String hospitalName;
+	
+//	no-args Constructor
+	public Doctor() {
+		// TODO Auto-generated constructor stub
+	}
+
+//	parameterized Constructor
+	public Doctor(String docName, double fees, String degree, int experience, String hospitalName) {
+		
+		this.docName = docName;
+		this.fees = fees;
+		this.degree = degree;
+		this.experience = experience;
+		this.hospitalName = hospitalName;
+	}
+	
+//	Behaviors
+	public void displayDoctor() {
+		System.out.println("----|| Doctor Details ||---");
+		System.out.println("Doctors Name : " + docName);
+		System.out.println("Fees : " + fees);
+		System.out.println("Degree : " + degree);
+		System.out.println("Experience : " + experience);
+		System.out.println("Hospital Name : " + hospitalName);
+	}
+	
+	
+}
