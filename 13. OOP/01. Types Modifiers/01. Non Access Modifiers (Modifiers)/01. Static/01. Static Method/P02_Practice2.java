b69254008bf46fad413274dd3242e1f1da508c6b
Ganesh Dhondiram Ugale <ugale.ganesh.d@gmail.com>
2025-06-22 22:29:20 +0530

    Update P02_Practice2.java

--- 13. OOP/01. Types Modifiers/01. Non Access Modifiers (Modifiers)/01. Static/01. Static Method/P02_Practice2.java
+++ 13. OOP/01. Types Modifiers/01. Non Access Modifiers (Modifiers)/01. Static/01. Static Method/P02_Practice2.java
@@ -2,6 +2,6 @@ class P02_Practice2{
 
 	public static void main(String[] args) {
 		System.out.print(P02_Practice1.a);
-
+// Here we try to callings the static method from another class as a class name as a ref.
 	}
 }
