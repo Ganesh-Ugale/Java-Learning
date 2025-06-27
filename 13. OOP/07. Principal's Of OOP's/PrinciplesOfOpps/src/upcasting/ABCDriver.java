39aa3c80e5d07fee7c4be4090b576a7c395d7f11
Ganesh-Ugale <ugale.ganesh.d@gmail.com>
2025-06-27 19:47:08 +0530

    How Upcasting is Working

---
+++ 13. OOP/07. Principal's Of OOP's/PrinciplesOfOpps/src/upcasting/ABCDriver.java
@@ -0,0 +1,21 @@
+package upcasting;
+
+public class ABCDriver {
+	public static void main(String[] args) {
+		
+//		ClassA a = new ClassB();
+//		ClassB b = (ClassB)a;
+		
+//		ClassA a = new ClassC();
+//		ClassC c = (ClassC)a;
+		
+//		ClassA a = new ClassB();
+//		ClassC c = (ClassC)a;
+		
+//		ClassA a = new ClassC();
+//		ClassB b = (ClassB)a;
+		
+//		ClassB b = new ClassC();
+//		ClassC c = (ClassC)b;
+	}
+}
