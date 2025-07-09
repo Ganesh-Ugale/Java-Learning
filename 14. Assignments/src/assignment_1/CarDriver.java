6a1b3f68742aa695b66c8bb5e82683b88763e241
Ganesh-Ugale <ugale.ganesh.d@gmail.com>
2025-07-09 09:13:10 +0530

    Car Driver

---
+++ 14. Assignments/src/assignment_1/CarDriver.java
@@ -0,0 +1,10 @@
+package assignment_1;
+
+public class CarDriver {
+	public static void main(String[] args) {
+		
+		Car c1 = new Car("Range Rover Sport", "Firenze Red", "250 kmph");
+		
+		c1.displayCar();
+	}
+}
