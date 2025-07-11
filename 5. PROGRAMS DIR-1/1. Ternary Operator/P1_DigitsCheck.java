2ced584e2e3a94db8d4896f104db5c3f4cf638d0
Ganesh-Ugale <ugale.ganesh.d@gmail.com>
2025-07-11 22:00:20 +0530

    Update P1_DigitsCheck.java

--- 5. PROGRAMS DIR-1/1. Ternary Operator/P1_DigitsCheck.java
+++ 5. PROGRAMS DIR-1/1. Ternary Operator/P1_DigitsCheck.java
@@ -8,7 +8,7 @@ class DigitsCheck
 {
 	public static void main(String[] args)
 	{
-		System.out.print("Enter a Digite : ");
+		System.out.print("Enter a Digit : ");
 		char ch = new Scanner(System.in).next().charAt(0);
 
 		String op = (ch>='0' && ch<='9')?(ch + " is a digit"):(ch + " is not a digit");
