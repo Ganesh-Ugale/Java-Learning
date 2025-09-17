e387adf109460097a3e423b6b54bad410347384c
Ganesh-Ugale <ugale.ganesh.d@gmail.com>
2025-09-17 12:04:51 +0530

    Create Son.java

---
+++ 13. OOP/07. Principal's Of OOP's/PrinciplesOfOpps/src/polymorphism/runtime/Son.java
@@ -0,0 +1,12 @@
+package polymorphism.runtime;
+
+public class Son extends Father{
+
+	public void finance() {
+		System.out.println("Rich...");
+	}
+	
+	public void name() {
+		System.out.println("Good Boy...");
+	}
+}
