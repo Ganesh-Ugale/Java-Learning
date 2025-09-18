efc7036165914f8e0dfaef15699e1f37646a46a6
Ganesh-Ugale <ugale.ganesh.d@gmail.com>
2025-09-18 12:04:26 +0530

    Create HpPrinter.java
    Why we Need Abstract Classes

---
+++ 13. OOP/07. Principal's Of OOP's/PrinciplesOfOpps/src/abtractmodifier/HpPrinter.java
@@ -0,0 +1,17 @@
+package abtractmodifier;
+
+public abstract class HpPrinter {
+	
+/*
+ * This is HP Printer, Here this is Incomplete class this only specify the 
+   features they needs, then service providers are implement the method, as per
+   there specifications, but runs on the name Hp Printers. i.e Gendralization
+ * */
+	
+	public HpPrinter() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public abstract void print();
+	
+}
