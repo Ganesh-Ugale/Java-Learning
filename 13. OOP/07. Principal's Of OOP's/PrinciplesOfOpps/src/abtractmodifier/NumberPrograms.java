236e6d4949a2cee0e9e8d137b3a79af5a9928fb8
Ganesh-Ugale <ugale.ganesh.d@gmail.com>
2025-09-19 12:02:28 +0530

    Create NumberPrograms.java

---
+++ 13. OOP/07. Principal's Of OOP's/PrinciplesOfOpps/src/abtractmodifier/NumberPrograms.java
@@ -0,0 +1,14 @@
+package abtractmodifier;
+
+// This is an Service Specifier class
+
+public abstract class NumberPrograms {
+	
+	public abstract boolean isPrime(int num);
+	public abstract int factorial(int num);
+	public abstract float pow(float base, float raise);
+	
+	public static float abc(int a) {
+		return 0;
+	}
+}
