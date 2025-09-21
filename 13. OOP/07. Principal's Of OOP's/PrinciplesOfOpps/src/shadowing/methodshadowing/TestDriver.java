1b60e341d631d63de4a1e3b14bd7cca6d4fe6e95
Ganesh-Ugale <ugale.ganesh.d@gmail.com>
2025-09-21 12:03:06 +0530

    Create TestDriver.java

---
+++ 13. OOP/07. Principal's Of OOP's/PrinciplesOfOpps/src/shadowing/methodshadowing/TestDriver.java
@@ -0,0 +1,17 @@
+package shadowing.methodshadowing;
+
+public class TestDriver {
+	
+	public static void main(String[] args) {
+		
+//		Test1 t1 = new Test2(); // up casting
+//		
+//		t1.demo(); // method shadowing
+//		t1.test(); // method overriding
+//		
+//		System.out.println(t1.a);
+//		System.out.println(t1.b);
+		
+	
+	}
+}
