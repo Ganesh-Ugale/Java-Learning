7e9eb81cb61c80a3eb7419207fb0af867ed8b3cc
Ganesh-Ugale <ugale.ganesh.d@gmail.com>
2025-09-28 15:15:58 +0530

    Create ClassB.java

---
+++ 13. OOP/07. Principal's Of OOP's/AccessModifiers/src/testpackage/ClassB.java
@@ -0,0 +1,14 @@
+package testpackage;
+
+/*
+ * Accessing Private Members
+ * 
+ * */
+
+import privateaccessmodifier.*;
+public class ClassB {
+	public static void main(String[] args) {
+//		Static Members
+//		Example1.
+	}
+}
